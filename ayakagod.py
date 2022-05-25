efa3b1c9a54241714d108608a10dfad62c054a55
Chokoramo <vzchoko15@gmail.com>
2022-05-25 00:36:43 -0500

    commit on git hub

--- ayakagod.py
+++ ayakagod.py
@@ -1,5 +1,18 @@
+import googletrans
+import lightbulb
 import hikari
 
-bot = hikari.GatewayBot(token='OTc4ODMzOTIzODcyMjcyMzg0.GowF-e.xaQLY98OToMZf-lzXVHdOcE0_sH9VVF0HZujO8')
-bot.run()
+bot = lightbulb.BotApp(token='OTc4ODMzOTIzODcyMjcyMzg0.GowF-e.xaQLY98OToMZf-lzXVHdOcE0_sH9VVF0HZujO8', 
+default_enabled_guilds=(978839879255490664))
 
+@bot.listen(hikari.StartedEvent)
+async def on_started(event):
+    print ('Lets translate')
+
+@bot.command
+@lightbulb.command('translate', 'On it!')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def Translate(ctx):
+    await ctx.respond('On it!')
+
+bot.run()
