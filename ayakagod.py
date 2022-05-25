015309c16320d94d50c361caf669f12e7588d785
Chokoramo <vzchoko15@gmail.com>
2022-05-25 11:10:22 -0500

    Mensajes

--- ayakagod.py
+++ ayakagod.py
@@ -2,18 +2,17 @@ import googletrans
 import lightbulb
 import hikari
 
-bot = lightbulb.BotApp(token='OTc4ODMzOTIzODcyMjcyMzg0.GowF-e.xaQLY98OToMZf-lzXVHdOcE0_sH9VVF0HZujO8', 
-default_enabled_guilds=(978839879255490664))
+bot = lightbulb.BotApp(token= 'OTc4ODMzOTIzODcyMjcyMzg0.GpnhW4.UAifPG6NuEYViOFXyY_HP_yEllnnUdCaF020Zk',                                                                                                                                                              
+ default_enabled_guilds=(978839879255490664))
 
 @bot.listen(hikari.StartedEvent)
 async def on_started(event):
     print ('Lets translate')
 
 @bot.command
-@lightbulb.command('translate', 'On it!')
+@lightbulb.command('translate', 'This will translate a short sentence said by a member of the voice chat!')
 @lightbulb.implements(lightbulb.SlashCommand)
-async def Translate(ctx):
-    await ctx.respond('On it!')
+async def Translate(ctx):   
+    await ctx.respond('I can hear you now!')
 
 bot.run()
-
